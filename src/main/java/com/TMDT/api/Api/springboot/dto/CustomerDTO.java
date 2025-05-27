f60c94c05d9fc94dc7f058206ff6bdfb34a2f015
truongthinh <21130547@st.hcmuaf.edu.vn>
2025-05-27 22:17:05 +0700

    fix login

--- src/main/java/com/TMDT/api/Api/springboot/dto/CustomerDTO.java
+++ src/main/java/com/TMDT/api/Api/springboot/dto/CustomerDTO.java
@@ -9,9 +9,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Getter
+@Setter
 public class CustomerDTO {
     private int id;
     private String username;
