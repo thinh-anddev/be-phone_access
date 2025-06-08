f579f6e24840a9add409fbfb7ec336189d81822c
truongthinh <21130547@st.hcmuaf.edu.vn>
2025-06-08 16:34:58 +0700

    update category

--- src/main/java/com/TMDT/api/Api/springboot/controllers/CategoryControllers.java
+++ src/main/java/com/TMDT/api/Api/springboot/controllers/CategoryControllers.java
@@ -1,5 +1,7 @@
 package com.TMDT.api.Api.springboot.controllers;
 
+import com.TMDT.api.Api.springboot.dto.CategoryDTO;
+import com.TMDT.api.Api.springboot.mapper.CategoryMapper;
 import com.TMDT.api.Api.springboot.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping(path = "/api/v1/categories")
 public class CategoryControllers {
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private CategoryMapper categoryMapper;
 
     @GetMapping
-    public ResponseEntity<ResponseObject> getAll(){
-        return ResponseEntity.ok(new ResponseObject("ok", "Success", categoryService.getAll()));
+    public ResponseEntity<ResponseObject> getAll() {
+        List<CategoryDTO> categories = categoryMapper.toListDto(categoryService.getAll());
+        return ResponseEntity.ok(new ResponseObject("ok", "Success", categories));
     }
 }
