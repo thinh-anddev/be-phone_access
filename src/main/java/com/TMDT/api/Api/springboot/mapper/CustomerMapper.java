9d8ae67fdeba8d899442a7849c1d5f636f53cbb0
thuynehe <nguyenthithuythuy123@gmail.com>
2025-06-08 23:07:03 +0700

    fix: customerMapper

--- src/main/java/com/TMDT/api/Api/springboot/mapper/CustomerMapper.java
+++ src/main/java/com/TMDT/api/Api/springboot/mapper/CustomerMapper.java
@@ -3,12 +3,16 @@ package com.TMDT.api.Api.springboot.mapper;
 import com.TMDT.api.Api.springboot.dto.*;
 import com.TMDT.api.Api.springboot.models.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
+    @Mapping(target = "orders", ignore = true)
+    @Mapping(target = "addresses", ignore = true)
+    @Mapping(target = "cartDetails", ignore = true)
     CustomerDTO toDto(Customer customer);
 
     Customer toEntity(CustomerDTO customerDTO);
